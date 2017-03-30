8d9b01ea003b163ef21dd3125b403616fd0d47b8
Jacek Suchenia <jacek.suchenia@ocado.com>
2017-03-30 15:49:46 +0200

    Expose page with basic information

--- src/main/java/com/ocado/devops/PptestApplication.java
+++ src/main/java/com/ocado/devops/PptestApplication.java
@@ -1,11 +1,27 @@
 package com.ocado.devops;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PptestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PptestApplication.class, args);
 	}
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
 }
